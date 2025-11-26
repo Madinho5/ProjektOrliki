fd8fec48cd65412bdb343343d58a4f11d45e791f
Madinho5 <strzeleckip11@gmail.com>
2025-11-26 19:30:09 +0100

    Initial commit

---
+++ src/main/java/com/example/ProjektOrliki/ProjektOrlikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjektOrliki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektOrlikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektOrlikiApplication.class, args);
+	}
+
+}
