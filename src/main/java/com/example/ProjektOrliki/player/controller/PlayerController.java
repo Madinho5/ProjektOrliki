1729c1d8dfa01a81a7073126635347cbbb870d8f
St4chu5 <marcel2003@gmail.com>
2025-11-29 20:18:38 +0100

    fix player

--- src/main/java/com/example/ProjektOrliki/player/controller/PlayerController.java
+++ src/main/java/com/example/ProjektOrliki/player/controller/PlayerController.java
@@ -1,10 +1,14 @@
 package com.example.ProjektOrliki.player.controller;
 
 import com.example.ProjektOrliki.player.dto.PlayerRequest;
+import com.example.ProjektOrliki.player.dto.PlayerResponse;
 import com.example.ProjektOrliki.player.model.Player;
 import com.example.ProjektOrliki.player.model.PlayerPosition;
+import com.example.ProjektOrliki.player.repository.PlayerRepository;
 import com.example.ProjektOrliki.player.service.PlayerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +19,25 @@ import java.util.List;
 public class PlayerController {
 
     private final PlayerService playerService;
+    private final PlayerRepository playerRepository;
 
     @PostMapping
-    public Player create(@RequestBody PlayerRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public PlayerResponse createPlayer(@RequestBody PlayerRequest request) {
         return playerService.createPlayer(request);
     }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PlayerResponse updatePlayer(@PathVariable Long id, @RequestBody PlayerRequest request) {
+        return playerService.updatePlayer(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePlayer(@PathVariable Long id) {
+        playerService.deletePlayer(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Usunięto zawodnika o id: " + id);
+    }
+
 }
 
